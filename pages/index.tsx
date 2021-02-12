375897e253c6f6d611d0f88c5776cc27db4fd289
Guilherme Santos <guilherme.dos.santos.91@gmail.com>
2021-02-11 21:43:15 -0300

    feat: add initial project files

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// Modules
+import { Button } from "@material-ui/core";
+
+const HomePage: NextPage = () => (
+  <Button color="primary" variant="contained">
+    My App
+  </Button>
+);
+
+export default HomePage;
